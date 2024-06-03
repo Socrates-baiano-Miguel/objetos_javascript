09e33faa86a157da65575c24ad0ed764999f908e
Socrates-baiano-Miguel <128157117+Socrates-baiano-Miguel@users.noreply.github.com>
2024-06-03 09:07:16 -0300

    Add files via upload

---
+++ aula1/acess.js
@@ -0,0 +1,12 @@
+
+let objectpessoal = {
+    nome: "Miguel",
+    idade: 18,
+    cpf: "989804937294873",
+    email: "migs@example.com",
+
+}
+ console.log(`Nome do Felizardo é ${objectpessoal.nome}
+// e a idade do caboco é de ${objectpessoal.idade} anos e os s 3 primeiros números do cpf ${objectpessoal.cpf.substring(12,15)} `)
+
+
