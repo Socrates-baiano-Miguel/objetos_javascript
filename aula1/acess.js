bf44e6f87ed68ee2984e379c90ea7dcd376bfa92
Socrates-baiano-Miguel <128157117+Socrates-baiano-Miguel@users.noreply.github.com>
2024-06-03 14:27:36 -0300

    Add files via upload

--- aula1/acess.js
+++ aula1/acess.js
@@ -6,7 +6,13 @@ let objectpessoal = {
     email: "migs@example.com",
 
 }
- console.log(`Nome do Felizardo é ${objectpessoal.nome}
-// e a idade do caboco é de ${objectpessoal.idade} anos e os s 3 primeiros números do cpf ${objectpessoal.cpf.substring(12,15)} `)
+ console.log(`Nome do Felizardo é ${objectpessoal.nome} e a idade do caboco é de ${objectpessoal.idade} anos e os s 3 primeiros números do cpf ${objectpessoal.cpf.substring(12,15)} `) 
 
+ let keys  = ['nome', 'idade', 'cpf', 'email']
+
+ keys.forEach ((key) => {
+    console.log(`Nome do Felizardo é ${objectpessoal["nome"]} e a idade do caboco é de ${objectpessoal["idade"]}`) 
+ } 
+
+);
 
---
+++ aula1/alterar.js
@@ -0,0 +1,10 @@
+let people = {
+    name: "Miguel",
+    work: "progamação front-end"
+
+}
+console.log(people.phone);
+
+people.phone = "43 666 666 666"
+
+console.log(people.phone);
