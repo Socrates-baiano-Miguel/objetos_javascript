671003230658762501cbcc338596f7bd8a0664f0
Socrates-baiano-Miguel <128157117+Socrates-baiano-Miguel@users.noreply.github.com>
2024-06-10 14:04:02 -0300

    Add files via upload

---
+++ A02/aninhados.js
@@ -0,0 +1,15 @@
+let client = {
+    name: "Miguel",
+    age: 18,
+    cpf: "989804937294873",
+    email: "migs@example.com",
+    phone: ["(43)666666666", "40028922"],
+};
+client.address = {
+    road:"Vereador Domingos Lourenço",
+    Number:"12",
+    apartment:"not",
+    complement:"perto da APAE",
+};
+
+console.log(client.address);
